d6955949b97953b9199fe5676739b78994687642
Stublink <chrisrey95@gmail.com>
2018-12-06 17:34:03 -0500

    added test for '/names' post route

--- api/server.js
+++ api/server.js
@@ -4,8 +4,32 @@ const server = express();
 
 server.use(express.json());
 
+const names = [
+    'Name 1',
+    'Name 2',
+    'Name 3',
+    'Name 4',
+    'Name 5',
+    'Name 6',
+    'Name 7',
+];
+
 server.get('/', (req, res) => {
     res.status(200).json({server: 'is up'})
+})
+
+server.get('/names', (req, res) => {
+    res.status(200).json(names);
+})
+
+server.post('/names', (req, res) => {
+    if (!req.body.name) {
+        res.status(400).json({error: 'name must be provided'})
+        return;
+    }
+    names.push(req.body.name);
+    const index = names.length - 1;
+    res.status(201).json({index});
 })
 
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -15,26 +15,66 @@ describe('server', () => {
         });
     });
 
-    describe('/names route', () => {
-        it('should return array of strings', async () => {
+    describe('/names get route', () => {
+        it('returns status 200', async () => {
             let response = await request(server).get('/names');
             expect(response.status).toBe(200);
-            expect(typeof response.data).toEqual('Array');
-            expect(typeof response.data[0]).toEqual('String');
+        });
+        
+        it('should return array of strings', async () => {
+            let response = await request(server).get('/names');
+            expect(typeof response.body[0]).toEqual('string');
         });
     });
 
     describe('/names post route', () => {
-        it('should return array of names', async () => {
-            let response = await request(server).get('/names');
-            expect(response.status).toBe(200);
+        it('should return index of new name', async () => {
+            const name = "new name"
+            let response = await request(server).post('/names').send({name});
+            const index = response.body.index;
+            
+            expect(response.status).toBe(201);
+            expect(typeof index).toEqual('number');
+
         });
+
+        it('index returned mathes index in db', async () => {
+            const name = "new name"
+            let response = await request(server).post('/names').send({name});
+            
+            expect(response.status).toBe(201);
+
+            const index = response.body.index;
+            response = await request(server).get('/names')
+            const names = response.body;
+
+            expect(names[index]).toEqual(name);
+        });
+
+        it('should respond with status code 400 if name is not provided', async () => {
+            let response = await request(server).post('/names').send({});
+            expect(response.status).toBe(400);
+        });
+
     });
 
     describe('/names delete route', () => {
-        it('should return array of names', async () => {
-            let response = await request(server).get('/names');
-            expect(response.status).toBe(200);
+        it('returns status 200 when delete is succesful', async () => {
+            const response = await request(server).delete('/names').send({index: 0});
+
+            expect(response.status).toEqual(200)
         });
+        
+        it('should return 404 if index cant be found', async () => {
+
+        });
+
+        it('should return 400 if index is not provided', () => {
+            
+        });
+
+        // return 200 on succesful delete
+        // return 404 if index is not found
+        // return 400 if index is not procided
     });
 });
